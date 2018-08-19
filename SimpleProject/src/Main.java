8faccc63700f9a1042312a2a8d5d365a14b742a8
Matthias Grandisch <matthias@grandisch.de>
2018-08-19 23:53:04 +0200

    2 Klassen erstellt und ausgeführt

--- SimpleProject/src/Main.java
+++ SimpleProject/src/Main.java
@@ -2,7 +2,13 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Output out1=new  Output();
+		Output out2=new  Output();
+		
+		out1.setText1("Hallo");
+		out2.setText1("Ihr da!");
+		out1.printText1();
+		out2.printText1();
 
 	}
 
--- SimpleProject/src/Output.java
+++ SimpleProject/src/Output.java
@@ -1,7 +1,11 @@
 
 public class Output {
 	private String text1;
+	
 	public void setText1(String text) {
 		text1 = text;
+	}
+	public void printText1() {
+		System.out.println(text1);
 	}
 }
