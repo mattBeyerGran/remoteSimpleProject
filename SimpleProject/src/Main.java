31e67a8fa8e1dde8d1664a33be7a712be93ce18d
Matthias Grandisch <matthias@grandisch.de>
2018-08-20 00:08:23 +0200

    3. Instanz

--- SimpleProject/src/Main.java
+++ SimpleProject/src/Main.java
@@ -4,11 +4,14 @@ public class Main {
 	public static void main(String[] args) {
 		Output out1=new  Output();
 		Output out2=new  Output();
+		Output out3=new  Output();
 		
 		out1.setText1("Hallo");
 		out2.setText1("Ihr da!");
+		out3.setText1("Was ist denn los?");
 		out1.printText1();
 		out2.printText1();
+		out3.printText1();
 
 	}
 
